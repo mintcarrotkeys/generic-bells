a29341b448256098f3df50de59338e858fe7f06b
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-01-10 23:31:57 +1100

    Add files via upload

--- src/components/DataMessage.js
+++ src/components/DataMessage.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { noteIcon } from "../assets/nav-icons";
+import {requestCode} from "../apiFetcher";
 
 
 export default function DataMessage(props) {
 
     const output = (
-        <div className="askLogin button" id="redirect_to_login">
+        <div className="askLogin button" id="redirect_to_login" onClick={requestCode}>
             {noteIcon}
             <div>
                 Login to view the latest data.
