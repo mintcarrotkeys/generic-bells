3971b8331f5c6e713b5b9d8b8318a6d6bd75b88c
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-01-13 20:13:16 +1100

    Update apiFetcher.js

--- src/apiFetcher.js
+++ src/apiFetcher.js
@@ -49,7 +49,7 @@ export async function requestToken() {
         }
     }
     const tokens = await response.json();
-    console.log(tokens);
+//     console.log(tokens);
     localStorage.setItem('handle_access', tokens['access_token']);
     localStorage.setItem('access_age', Date.now().toString());
 
