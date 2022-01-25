c510ff6ab4a61d7a3071b89709f727d94754ff19
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-01-25 23:45:28 +1100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { passItem, saveItem } from "./version";
 
 
-// let TESTDATA =
 
-
-// saveItem('storedData', TESTDATA);
-
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+window.location.assign("https://genericbells.pages.dev/");
